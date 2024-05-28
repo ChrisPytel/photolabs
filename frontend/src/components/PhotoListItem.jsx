4adc4e21e5f6250071074aa38557f884416efa39
ChrisPytel <chris_pytel@hotmail.com>
2024-05-28 18:31:35 -0400

    refactor sampleData out of photoListItem and into app.jsx and pass props down

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,26 +3,16 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
+  console.log(`Our props to photoListItem are: `, props);
 
   return(
-    <div className="imgContainer">
-      <img src={sampleDataForPhotoListItem.imageSource}></img>
+  <div className={`imgContainer id-${props.sampleData.id}`}>
+      <img src={props.sampleData.imageSource}></img>
       <div className="userInfo">
-      <img src={sampleDataForPhotoListItem.profile}></img>
-      <h3>{sampleDataForPhotoListItem.username}</h3>
-      <p>{sampleDataForPhotoListItem.location.city}, {sampleDataForPhotoListItem.location.country}</p>
+      <img src={props.sampleData.profile}></img>
+      <h3>{props.sampleData.username}</h3>
+      <p>{props.sampleData.location.city}, {props.sampleData.location.country}</p>
       </div>
     </div>
   );
