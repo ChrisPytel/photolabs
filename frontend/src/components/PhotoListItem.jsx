418448457fd688fd3e64773f886f1dc63e16abfa
ChrisPytel <chris_pytel@hotmail.com>
2024-05-28 22:24:21 -0400

    apply scss styling to photoListItem component by adding classNames

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../styles/PhotoListItem.scss";
 
@@ -6,16 +6,25 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   console.log(`Our props to photoListItem are: `, props);
 
-  return(
-  <div className={`imgContainer id-${props.sampleData.id}`}>
-      <img src={props.sampleData.imageSource}></img>
-      <div className="userInfo">
-      <img src={props.sampleData.profile}></img>
-      <h3>{props.sampleData.username}</h3>
-      <p>{props.sampleData.location.city}, {props.sampleData.location.country}</p>
-      </div>
+  const [light, setLight] = useState(`off`);
+
+  console.log(light, setLight);
+
+
+return(
+  <div className={`photo-list__item id-${props.sampleData.id}`} key={props.sampleData.id}>
+    <img className="photo-list__image" src={props.sampleData.imageSource}/> 
+    <div className="photo-list__user-details">
+      <img className="photo-list__user-profile" src={props.sampleData.profile}/>         
+      <div className="photo-list__user-info">
+          <span>{props.sampleData.username}</span>
+          <div className="photo-list__user-location">
+            <span>{props.sampleData.location.city}, {props.sampleData.location.country}</span>
+          </div>
+        </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default PhotoListItem;
