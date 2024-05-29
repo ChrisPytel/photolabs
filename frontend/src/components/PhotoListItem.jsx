490ae718afd3801d17e9d791bb55a014f8315170
ChrisPytel <chris_pytel@hotmail.com>
2024-05-29 00:59:23 -0400

    update photoListItem.jsx to contain alt tags for images

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,25 +2,22 @@ import React, { useState } from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
 const PhotoListItem = (props) => {
-  console.log(`Our props to photoListItem are: `, props);
-
   const listItem = props.photoListData;
 
-  return(
-    <div className={`photo-list__item id-${listItem.id}`} key={listItem.id}>
-      <img className="photo-list__image" src={listItem.imageSource}/> 
-      <div className="photo-list__user-details">
-        <img className="photo-list__user-profile" src={listItem.profile}/>         
-        <div className="photo-list__user-info">
-            <span>{listItem.username}</span>
-            <div className="photo-list__user-location">
-              <span>{listItem.location.city}, {listItem.location.country}</span>
+  return(    
+      <div className={`photo-list__item id-${listItem.id}`} key={listItem.id}>
+        <img className="photo-list__image" src={listItem.imageSource} alt={`Photo credit: ${listItem.username}`}/> 
+        <div className="photo-list__user-details">
+          <img className="photo-list__user-profile" src={listItem.profile} alt={`${listItem.username} Profile Image`}/>         
+          <div className="photo-list__user-info">
+              <span>{listItem.username}</span>
+              <div className="photo-list__user-location">
+                <span>{listItem.location.city}, {listItem.location.country}</span>
+              </div>
             </div>
-          </div>
-      </div>
-    </div>
+        </div>
+      </div>    
   );
 };
 
