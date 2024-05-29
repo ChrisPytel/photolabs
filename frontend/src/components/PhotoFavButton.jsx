76af68d51e56f1e14c742ee130e41340742a51c4
ChrisPytel <chris_pytel@hotmail.com>
2024-05-29 19:42:02 -0400

    add a heart svg icon to PhotoFavButton.jsx and initialize state to toggle favourite status

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,11 +3,18 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {  
+  const [favourite, setFavourite] = useState(false);
+  
+  const handleFavoritePress = function() {
+    setFavourite(favourite ? false  : true) 
+    console.log(`Status of favourite is:`, favourite);    
+  };
+
   return (
     <div className="photo-list__fav-icon">
-      <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+      <div className="photo-list__fav-icon-svg" onClick={handleFavoritePress}>
+        {<FavIcon selected={favourite}/>}
       </div>
     </div>
   );
