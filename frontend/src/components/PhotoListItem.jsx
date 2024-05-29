bc5646443f07bd0fbc373c50db1d9a6b0081820d
ChrisPytel <chris_pytel@hotmail.com>
2024-05-29 19:40:12 -0400

    import PhotoFavButton to PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   const listItem = props.photoListData;
 
   return(    
       <div className={`photo-list__item id-${listItem.id}`} key={listItem.id}>
+        <PhotoFavButton/>
         <img className="photo-list__image" src={listItem.imageSource} alt={`Photo credit: ${listItem.username}`}/> 
         <div className="photo-list__user-details">
           <img className="photo-list__user-profile" src={listItem.profile} alt={`${listItem.username} Profile Image`}/>         
