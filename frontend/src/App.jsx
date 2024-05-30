7c7a3faecc7ff2732434bd35e63950942886f830
ChrisPytel <chris_pytel@hotmail.com>
2024-05-30 12:22:13 -0400

    import PhotoList component and comment out sample data and array of 3 photoItems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
 import './App.scss';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+// const sampleDataForPhotoListItem = {
+//   id: "1",
+//   location: {
+//     city: "Montreal",
+//     country: "Canada",
+//   },
+//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+//   username: "Joe Example",
+//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+// };
 
-// const photos = new Array(3)
 
-const photos = [...Array(3)];
-const photoItems = photos.map((photoItem, index) => 
-  <PhotoListItem key={index} photoListData={sampleDataForPhotoListItem}/>
-);
 
+// const photos = [...Array(3)];
+// const photoItems = photos.map((photoItem, index) => 
+//   <PhotoListItem key={index} photoListData={sampleDataForPhotoListItem}/>
+// );
 
 
 // Note: Rendering a single component to build components in isolation
@@ -29,7 +29,8 @@ const App = () => {
   return (
     <div className="App">
       <div className="photo-list">
-      {photoItems}
+      {/* {photoItems} */}
+      <PhotoList/>
       </div>
     </div>
   );
