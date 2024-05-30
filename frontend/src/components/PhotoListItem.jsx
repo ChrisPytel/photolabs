589b6d76c564a4c7b4288b6d742421b27e461161
ChrisPytel <chris_pytel@hotmail.com>
2024-05-30 12:28:08 -0400

    update PhotoListItem.jsx to use newer props passed from PhotoList.jsx as its a newer dataset

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -9,11 +9,11 @@ const PhotoListItem = (props) => {
   return(    
       <div className={`photo-list__item id-${listItem.id}`} key={listItem.id}>
         <PhotoFavButton/>
-        <img className="photo-list__image" src={listItem.imageSource} alt={`Photo credit: ${listItem.username}`}/> 
+        <img className="photo-list__image" src={listItem.urls.full} alt={`Photo credit: ${listItem.username}`}/> 
         <div className="photo-list__user-details">
-          <img className="photo-list__user-profile" src={listItem.profile} alt={`${listItem.username} Profile Image`}/>         
+          <img className="photo-list__user-profile" src={listItem.user.profile} alt={`${listItem.username} Profile Image`}/>         
           <div className="photo-list__user-info">
-              <span>{listItem.username}</span>
+              <span>{listItem.user.name}</span>
               <div className="photo-list__user-location">
                 <span>{listItem.location.city}, {listItem.location.country}</span>
               </div>
