ef4e29fc120f3945b876573b68ae1c182a9cf158
ChrisPytel <chris_pytel@hotmail.com>
2024-05-31 01:11:56 -0400

    create TopicListItem.jsx and apply styles

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -8,10 +8,12 @@ const sampleDataForTopicListItem = {
   label: "Nature",
 };
 
-const TopicListItem = () => {
+const TopicListItem = (props) => {
+  // console.log(`Our props are: `, props);
+
   return (
     <div className="topic-list__item">
-      {/* Insert React */}
+        <span>{props.listItem.title}</span>
     </div>
   );
 };
