cdcd428e7a323b7b55bb7070819886754dc7f595
ChrisPytel <chris_pytel@hotmail.com>
2024-06-01 16:46:47 -0400

    refactor PhotoFavButton.jsx to remove local favourite state, migrate console.log out of useEffect and into onClick callback

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,20 +4,12 @@ import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton({globalFavourites, toggleFavourite, elementID}) {  
-  const [favourite, setFavourite] = useState(false);
   
   const handleFavoritePress = function() {
-  //  setFavourite(favourite ? false  : true)          // Option A - Ternary statement toggle
-  //  setFavourite(prevFavourite => !prevFavourite);   // Option B - Toggles based on previous state  
-   toggleFavourite(elementID);                         // Option C - Toggles Global favorite  
+   toggleFavourite(elementID);                                  //Toggles Global favorite  
+   console.log(`Status of favourite is:`, globalFavourites);
   };
   
-  useEffect(() => { 
-    // refactored console log out of handleFavoritePress as it might not always print 
-    // the updated state because state updates in React are asynchronous
-    console.log(`Status of favourite is:`, favourite);
-  }, [favourite]);
-
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg" onClick={handleFavoritePress}>
