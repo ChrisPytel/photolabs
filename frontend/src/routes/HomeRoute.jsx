e555e0692557d380ee85cae17b5a29a79eb00f25
ChrisPytel <chris_pytel@hotmail.com>
2024-05-31 20:31:13 -0400

    update HomeRoute.jsx to display nav and container with all photoListItems

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 import '../styles/HomeRoute.scss';
+import PhotoList from 'components/PhotoList';
+import TopNavigation from 'components/TopNavigationBar';
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigation/>
+      <div className="photo-list">        
+        <PhotoList/>
+      </div>
     </div>
   );
 };
