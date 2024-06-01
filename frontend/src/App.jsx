6d5988c0da893ef9fcebe8fdb0a9f97e3fef5329
ChrisPytel <chris_pytel@hotmail.com>
2024-05-31 20:32:55 -0400

    update App.jsx to show HomeRoute along with homepage, remove old comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,10 @@ import React from 'react';
 import './App.scss';
 // import PhotoListItem from './components/PhotoListItem';
 // import TopicListItem from 'components/TopicListItem';
-import PhotoList from './components/PhotoList';
-import TopicList from 'components/TopicList';
-import TopNavigation from 'components/TopNavigationBar';
-
-
-// const sampleDataForPhotoListItem = {
-//   id: "1",
-//   location: {
-//     city: "Montreal",
-//     country: "Canada",
-//   },
-//   imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-//   username: "Joe Example",
-//   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-// };
-
-
-
-// const photos = [...Array(3)];
-// const photoItems = photos.map((photoItem, index) => 
-//   <PhotoListItem key={index} photoListData={sampleDataForPhotoListItem}/>
-// );
+// import PhotoList from './components/PhotoList';
+// import TopicList from 'components/TopicList';
+// import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 
 
 // Note: Rendering a single component to build components in isolation
@@ -32,12 +14,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="photo-list">
-      {/* {photoItems} */}
-      {/* <PhotoList/> */}
-      {/* <TopicList/> */}
-      <TopNavigation/>      
-      </div>
+      <HomeRoute/>
     </div>
   );
 };
