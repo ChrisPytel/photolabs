789bbedea33c2cfb41b5bd8f8c4a019e533d164a
ChrisPytel <chris_pytel@hotmail.com>
2024-06-01 17:32:08 -0400

    create onClick for each <li> PhotoListItem to be able to setActiveModal to its current ID

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -3,11 +3,12 @@ import React from "react";
 import PhotoListItem from "./PhotoListItem";
 import "../styles/PhotoList.scss";
 
-const PhotoList = ({photos, globalFavourites, toggleFavourite}) => {
+const PhotoList = ({photos, globalFavourites, toggleFavourite, setActiveModal}) => {
     
   //Refactored Map away from using index and now uses photoItem.ID as its key identifier
   const photoItems = photos.map((photoItem) =>
-    <li key={photoItem.id}>
+
+    <li key={photoItem.id} onClick={()=>setActiveModal(photoItem.id)}>
       <PhotoListItem 
         photoListData={photoItem}
         globalFavourites={globalFavourites}
