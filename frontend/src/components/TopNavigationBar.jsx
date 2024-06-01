4c3bdf00dfa73e8a6cdf3de8c1cc8b35fb0eb483
ChrisPytel <chris_pytel@hotmail.com>
2024-06-01 16:53:04 -0400

    import FavIcon instead of FavBadge into TopNavigationBar.jsx and pass down correct props

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 
 import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
-import FavBadge from './FavBadge';
+import FavIcon from './FavIcon';
 
-const TopNavigation = (props) => {
-  // console.log(`Our TopNavigation props: `, props);
+const TopNavigation = ({topics, globalFavourites}) => {
 
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={props.topics}/>
-      <FavBadge/>
+      <TopicList topics={topics}/>
+      <FavIcon displayAlert={(globalFavourites.length > 0)} selected={true}/>
     </div>
   )
 }
