4dc9574d3af05e18c456e9bb38f87ecf0fa45f53
ChrisPytel <chris_pytel@hotmail.com>
2024-06-02 12:44:30 -0400

    create TestFile component from local directory on Laptop

---
+++ frontend/src/components/TestFile.jsx
@@ -0,0 +1,16 @@
+// Test file from local directory Chris - Laptop
+
+import React from "react";
+
+
+const TestFile = (props) => {
+   console.log(`Our props: `, props);
+
+  return (
+    <div>
+      <p>Hello!</p>
+    </div>
+  );
+};
+
+export default TestFile;
