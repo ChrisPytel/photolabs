47caee28c897367f6c0550b7fb30faab58b6534b
ChrisPytel <chris_pytel@hotmail.com>
2024-06-02 13:30:35 -0400

    update PhotoDetailsModal.jsx to take prop details from the selectedImage state from parent in App.jsx

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({setActiveModal}) => {
-  
+const PhotoDetailsModal = ({selectedImage, toggleModal}) => {
+  console.log(`Current modal is recieving: `, selectedImage);
+
   return (
     <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button" onClick={()=> setActiveModal(false)}>
+      <button className="photo-details-modal__close-button" onClick={()=> toggleModal()}>
         <img src={closeSymbol} alt="close symbol" />
+        {/* <PhotoListItem /> */}
       </button>
     </div>
   )
