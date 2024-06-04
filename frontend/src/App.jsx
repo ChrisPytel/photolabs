57eba3f6b6687884469f46cd6fa6301456a08fbe
ChrisPytel <chris_pytel@hotmail.com>
2024-06-03 23:50:00 -0400

    refactor App.jsx to useApplicationData hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,25 +10,18 @@ import photos from 'mocks/photos';
 //Imported Components
 import HomeRoute from 'routes/HomeRoute';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import useApplicationData from './hooks/useApplicationData';  //Custom react hook
+
 
 const App = () => {
 
-  //States
-  const [activeModal, setActiveModal] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(false);
-  const [globalFavourites, setGlobalFavourites] = useState([]);
-  
-  const toggleFavourite = (photoID) => {
-    globalFavourites.includes(photoID)?                                              // checks if photoID exists that was clicked exists in state
-    setGlobalFavourites(globalFavourites.filter((element) => element !== photoID)):  // If it exists, it filters it out of the array and removes it
-    setGlobalFavourites([...globalFavourites, photoID])                              // If it doesnt exist, spreads current favorites and adds it at the end 
-  };
-
-  //Function for toggling and passing in details for iamge to modal
-  const toggleModal = function(photoDetails) { 
-    setActiveModal(!activeModal); //toggles modal status
-    setSelectedImage(photoDetails);       
-  };
+  //Refactored state and helpers into useApplicationData.jsx
+  const { 
+    activeModal, 
+    selectedImage, 
+    globalFavourites, 
+    toggleFavourite, 
+    toggleModal} = useApplicationData();
     
   useEffect(() => console.log(`UseEffect says globalFavourites is: `, globalFavourites), [globalFavourites]); 
   useEffect(() => console.log(`UseEffect says activeModal is: `, activeModal), [activeModal]); 
