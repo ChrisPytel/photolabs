96c4c8f9978bbb1b10055c81f3bcff530d024833
ChrisPytel <chris_pytel@hotmail.com>
2024-06-05 13:15:10 -0400

    refactor useApplicationData hook to utilize useReducer over handling multiple independant states

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,4 +1,74 @@
-import React, {useState} from 'react'
+import { useReducer } from 'react';
+
+const INITIAL_STATE = {
+  globalFavourites: [],
+  selectedImage: false,
+  activeModal: false
+};
+
+const ACTIONS = {
+  TOGGLE_FAVOURITE: 'TOGGLE_FAVOURITE',
+  SELECT_IMAGE: 'SELECT_IMAGE',
+  CLOSE_MODAL: 'CLOSE_MODAL'
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+
+    case ACTIONS.TOGGLE_FAVOURITE:
+      const photoID = action.payload;
+
+      // checks if photoID exists that was clicked exists in state
+      if (state.globalFavourites.includes(photoID)) {   
+         // If it exists, it filters it out of the array and removes it                    
+        return {...state, globalFavourites: state.globalFavourites.filter(element => element !== photoID)};
+      }
+      // If it doesnt exist, spreads current favorites and adds it at the end 
+      return {...state, globalFavourites: [...state.globalFavourites, photoID]};
+
+    case ACTIONS.SELECT_IMAGE:
+      return {...state, activeModal: true, selectedImage: action.payload};
+
+    case ACTIONS.CLOSE_MODAL:
+      return {...state, activeModal: false, selectedImage: null};
+
+    default:
+      throw new Error(`Invalid action type: ${action.type}`);
+  }
+};
+
+function useApplicationData() {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+
+
+  const toggleFavourite = function(photoID) {
+    dispatch({type: ACTIONS.TOGGLE_FAVOURITE, payload: photoID})       
+  };
+
+  const toggleModal = function(photoDetails) {
+    if (state.activeModal) {
+      dispatch({type: ACTIONS.CLOSE_MODAL});
+    } else {
+      dispatch({type: ACTIONS.SELECT_IMAGE, payload: photoDetails});
+    }   
+  };
+
+  return {
+    activeModal: state.activeModal,
+    selectedImage: state.selectedImage,
+    globalFavourites: state.globalFavourites,
+    toggleFavourite,
+    toggleModal
+  };
+}
+
+export default useApplicationData;
+
+
+
+// --------------- Before useReducer refactor: Original states and helper functions ---------------
+
+/* import React, {useState} from 'react'
 
 function useApplicationData() {
 
@@ -23,4 +93,4 @@ function useApplicationData() {
 
 }
 
-export default useApplicationData
+export default useApplicationData; */
